7c1704f0189920886a96ff6eb0c2959633f9435a
findrr xplore <findrr.hooker.xplorer@gmail.com>
2019-09-29 00:52:04 -0300

    compoonetization content in any parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { IntroComponent } from './intro/intro.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContentComponent } from './content/content.component';
 import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,11 @@ import { TestimonialComponent } from './testimonial/testimonial.component';
     IntroComponent,
     GalleryComponent,
     ContentComponent,
-    TestimonialComponent
+    TestimonialComponent,
+    FooterComponent,
+    ClientsComponent,
+    PricingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
