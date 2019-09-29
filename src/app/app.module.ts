edb889a66c932bc1a3a5fc90e731efefc5779845
findrr xplore <findrr.hooker.xplorer@gmail.com>
2019-09-28 21:12:20 -0300

    create with content ContentConmponent and testimnonialComponent create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
-    GalleryComponent
+    GalleryComponent,
+    ContentComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule
